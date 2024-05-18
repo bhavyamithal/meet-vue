2c1f9cebcaad08ff49ee2ad0f5903219666b24d3
bhavyamithal <bm820@snu.edu.in>
2024-05-18 11:15:15 +0530

    implement personal meeting room

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -62,7 +62,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
         if (type === 'recordings') {
             fetchRecordings();
         }
-    }, [type, callRecordings]);
+    }, [type, callRecordings, toast]);
 
     if (isLoading) return <Loader />;
 
@@ -85,7 +85,7 @@ const CallList = ({ type }: { type: 'ended' | 'upcoming' | 'recordings' }) => {
                         title={
                             (meeting as Call).state?.custom?.description ||
                             (meeting as CallRecording).filename?.substring(0, 20) ||
-                            'No Description'
+                            'Personal Meeting'
                         }
                         date={
                             (meeting as Call).state?.startsAt?.toLocaleString() ||
